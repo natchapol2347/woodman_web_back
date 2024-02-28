5292b3a1ca9d302aaff920ef4a7e5e77e302f6ea
Natchapol Watthanawong <a677022@AR8040677022N1.local>
2024-02-28 11:25:20 +0700

    initial commit with model

---
+++ handler/handler.go
@@ -0,0 +1,13 @@
+package handler
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+// func (h *Handler) Connex() (int, error) {
+
+// 	return 3,nil
+// }
---
+++ model/model.go
@@ -0,0 +1,73 @@
+package model
+
+import "time"
+
+// CREATE TABLE IF NOT EXISTS Portfolio (
+//     ProjectID INT PRIMARY KEY AUTO_INCREMENT,
+//     ProjectName VARCHAR(255) NOT NULL,
+//     Description TEXT,
+//     CategoryID INT,
+//     TagID INT,
+//     CompletionDate DATE,
+//     FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID),
+//     FOREIGN KEY (TagID) REFERENCES Tag(TagID)
+// );
+// CREATE TABLE IF NOT EXISTS PortfolioImages (
+//     ImageID INT PRIMARY KEY AUTO_INCREMENT,
+//     ProjectID INT NOT NULL,
+//     ImageURL VARCHAR(255) NOT NULL,
+//     FOREIGN KEY (ProjectID) REFERENCES Portfolio(ProjectID)
+// );
+// -- ContactForm Table
+// CREATE TABLE IF NOT EXISTS ContactForm (
+//
+//	SubmissionID INT PRIMARY KEY AUTO_INCREMENT,
+//	Name VARCHAR(255) NOT NULL,
+//	Email VARCHAR(255) NOT NULL,
+//	Message TEXT NOT NULL,
+//	Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//
+// );
+
+// CREATE TABLE IF NOT EXISTS Job (
+//     JobID INT PRIMARY KEY AUTO_INCREMENT,
+//     Title VARCHAR(255) NOT NULL,
+//     Description TEXT,
+//     Requirements TEXT,
+//     Location VARCHAR(255),
+//     DatePosted DATE,
+//     Status VARCHAR(255) DEFAULT 'Open'
+// );
+
+type Portfolio struct {
+	ProjectID      int    `json:"projectID"`
+	ProjectName    string `json:"projectName"`
+	Description    string `json:"description"`
+	CompletionDate string `json:"completionDate"`
+	CategoryID     int    `json:"categoryID"` //     FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID),
+	TagID          int    `json:"TagID"`      //     FOREIGN KEY (TagID) REFERENCES Tag(TagID)
+}
+
+type PortfolioImages struct {
+	ImageID   int    `json:"imageID"`
+	ProjectID int    `json:"projectID"` //     		 FOREIGN KEY (ProjectID) REFERENCES Portfolio(ProjectID)
+	ImageUrl  string `json:"imageUrl"`
+}
+
+type ContactForm struct {
+	SubmissionID int       `json:"submissionID"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	Message      string    `json:"message"`
+	Timestamp    time.Time `json:"timeStamp"`
+}
+
+type Job struct {
+	JobID        int    `json:"jobID"`
+	Title        string `json:"title"`
+	Description  string `json:"description"`
+	Requirements string `json:"requirements"`
+	Location     string `json:"location"`
+	DatePosted   string `json:"datePosted"`
+	Status       string `json:"status"`
+}
